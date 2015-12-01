48a1c5304c7b6b901e6e1458a396ac75f8d6a8ad
stevenjdunn <mail@stevendunn.co.uk>
2015-12-01 13:08:46 +0000

    Create autospades.py
    A simple script to automatically assemble paired-end reads in SPAdes.

---
+++ autospades.py
@@ -0,0 +1,12 @@
+directory = raw_input("Path to directory containing FastQ reads: ")
+import os
+import glob
+r1files = list(glob.glob(os.path.join(directory,'*R1*.fastq')))
+r1files.sort()
+r2files = list(glob.glob(os.path.join(directory, '*R2*.fastq')))
+r2files.sort()
+rawname = [x.split(directory)[1].split('_')[0] for x in r1files]
+rawname.sort()
+import subprocess
+for r1, r2, out,in zip(r1files, r2files,rawname):
+    subprocess.check_call(['spades.py', '--pe1-1', r1, '--pe1-2', r2, '-o', out])
